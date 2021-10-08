0dd2a0ad64113cfaae0ed51fda0aab651e259916
Brody <brodyhoskins@users.noreply.github.com>
2021-10-08 13:33:10 -0700

    Add `#hazmat`, `#nmfc` to `Package`

--- lib/interstellar/package.rb
+++ lib/interstellar/package.rb
@@ -1,8 +1,9 @@
-module Interstellar #:nodoc:
+module Interstellar # :nodoc:
   class Package
     VALID_FREIGHT_CLASSES = [55, 60, 65, 70, 77.5, 85, 92.5, 100, 110, 125, 150, 175, 200, 250, 300, 400].freeze
 
     cattr_accessor :default_options
+    attr_accessor :nmfc, :hazmat
     attr_reader :currency, :options, :value
     attr_writer :declared_freight_class
 
@@ -22,22 +23,22 @@ module Interstellar #:nodoc:
         @dimensions = [@dimensions].flatten.reject(&:nil?)
       end
 
+      @hazmat = options[:hazmat] == true
+      @nmfc = options[:nmfc].blank? ? nil : options[:nmfc]
+
       imperial = (options[:units] == :imperial)
 
       weight_imperial = dimensions_imperial = imperial if options.include?(:units)
 
-      if options.include?(:weight_units)
-        weight_imperial = (options[:weight_units] == :imperial)
-      end
+      weight_imperial = (options[:weight_units] == :imperial) if options.include?(:weight_units)
 
-      if options.include?(:dim_units)
-        dimensions_imperial = (options[:dim_units] == :imperial)
-      end
+      dimensions_imperial = (options[:dim_units] == :imperial) if options.include?(:dim_units)
 
       @weight_unit_system = weight_imperial ? :imperial : :metric
       @dimensions_unit_system = dimensions_imperial ? :imperial : :metric
 
-      @weight = attribute_from_metric_or_imperial(grams_or_ounces, Measured::Weight, @weight_unit_system, :grams, :ounces)
+      @weight = attribute_from_metric_or_imperial(grams_or_ounces, Measured::Weight, @weight_unit_system, :grams,
+                                                  :ounces)
 
       if @dimensions.blank?
         zero_length = Measured::Length.new(0, (dimensions_imperial ? :inches : :centimetres))
@@ -57,7 +58,7 @@ module Interstellar #:nodoc:
 
       @value = Package.cents_from(options[:value])
       @currency = options[:currency] || (options[:value].currency if options[:value].respond_to?(:currency))
-      @cylinder = (options[:cylinder] || options[:tube]) ? true : false
+      @cylinder = options[:cylinder] || options[:tube] ? true : false
       @gift = options[:gift] ? true : false
       @oversized = options[:oversized] ? true : false
       @unpackaged = options[:unpackaged] ? true : false
@@ -115,45 +116,49 @@ module Interstellar #:nodoc:
       @unpackaged
     end
 
-    alias_method :tube?, :cylinder?
+    alias tube? cylinder?
 
     def gift?
       @gift
     end
 
+    def hazmat?
+      @hazmat
+    end
+
     def ounces(options = {})
       weight(options).convert_to(:oz).value.to_f
     end
-    alias_method :oz, :ounces
+    alias oz ounces
 
     def grams(options = {})
       weight(options).convert_to(:g).value.to_f
     end
-    alias_method :g, :grams
+    alias g grams
 
     def pounds(options = {})
       weight(options).convert_to(:lb).value.to_f
     end
-    alias_method :lb, :pounds
-    alias_method :lbs, :pounds
+    alias lb pounds
+    alias lbs pounds
 
     def kilograms(options = {})
       weight(options).convert_to(:kg).value.to_f
     end
-    alias_method :kg, :kilograms
-    alias_method :kgs, :kilograms
+    alias kg kilograms
+    alias kgs kilograms
 
     def inches(measurement = nil)
       @inches ||= @dimensions.map { |m| m.convert_to(:in).value.to_f }
       measurement.nil? ? @inches : measure(measurement, @inches)
     end
-    alias_method :in, :inches
+    alias in inches
 
     def centimetres(measurement = nil)
       @centimetres ||= @dimensions.map { |m| m.convert_to(:cm).value.to_f }
       measurement.nil? ? @centimetres : measure(measurement, @centimetres)
     end
-    alias_method :cm, :centimetres
+    alias cm centimetres
 
     def weight(options = {})
       case options[:type]
@@ -165,15 +170,16 @@ module Interstellar #:nodoc:
           @weight_unit_system == :imperial ? m.convert_to(:oz) : m
         end
       when :billable
-        [weight, weight(:type => :volumetric)].max
+        [weight, weight(type: :volumetric)].max
       end
     end
-    alias_method :mass, :weight
+    alias mass weight
 
     def self.cents_from(money)
       return nil if money.nil?
+
       if money.respond_to?(:cents)
-        return money.cents
+        money.cents
       else
         case money
         when Float
@@ -190,9 +196,9 @@ module Interstellar #:nodoc:
 
     def attribute_from_metric_or_imperial(obj, klass, unit_system, metric_unit, imperial_unit)
       if obj.is_a?(klass)
-        return obj
+        obj
       else
-        return klass.new(obj, (unit_system == :imperial ? imperial_unit : metric_unit))
+        klass.new(obj, (unit_system == :imperial ? imperial_unit : metric_unit))
       end
     end
 
@@ -235,8 +241,8 @@ module Interstellar #:nodoc:
       when :y, :mid, :width, :wide then ary[1]
       when :z, :min, :height, :depth, :high, :deep then ary[0]
       when :girth, :around, :circumference
-        self.cylinder? ? (Math::PI * (ary[0] + ary[1]) / 2) : (2 * ary[0]) + (2 * ary[1])
-      when :volume then self.cylinder? ? (Math::PI * (ary[0] + ary[1]) / 4)**2 * ary[2] : measure(:box_volume, ary)
+        cylinder? ? (Math::PI * (ary[0] + ary[1]) / 2) : (2 * ary[0]) + (2 * ary[1])
+      when :volume then cylinder? ? (Math::PI * (ary[0] + ary[1]) / 4)**2 * ary[2] : measure(:box_volume, ary)
       when :box_volume then ary[0] * ary[1] * ary[2]
       end
     end
