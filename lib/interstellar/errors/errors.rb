9409a2a8d6b17c30e11d2e282bb46ee5b6cdf475
Brody <brodyhoskins@users.noreply.github.com>
2021-12-13 22:02:26 -0800

    Add `UnserviceableAccessorialsError`

--- lib/interstellar/errors/errors.rb
+++ lib/interstellar/errors/errors.rb
@@ -1,14 +1,11 @@
 # frozen_string_literal: true
 
 require 'interstellar/errors/error'
-
 require 'interstellar/errors/http_error'
-
 require 'interstellar/errors/invalid_credentials_error'
 require 'interstellar/errors/expired_credentials_error'
-
 require 'interstellar/errors/document_not_found_error'
 require 'interstellar/errors/shipment_not_found_error'
-
 require 'interstellar/errors/response_error'
 require 'interstellar/errors/unserviceable_error'
+require 'interstellar/errors/unserviceable_accessorials_error'
