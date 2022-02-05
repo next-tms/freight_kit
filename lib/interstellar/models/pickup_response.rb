8187a700353a766fbe9aa9987655cae97866f372
Brody <brodyhoskins@users.noreply.github.com>
2022-02-05 11:36:37 -0800

    Add `Pickup#labels`

--- lib/interstellar/models/pickup_response.rb
+++ lib/interstellar/models/pickup_response.rb
@@ -5,11 +5,15 @@ module Interstellar
   # call. The most important method is {#pickup_number}, which will return the pickup reference
   # number.
   #
+  # @!attribute labels
+  #    Shipping labels.
+  #    @return [Array<Interstellar::Label>]
+  #
   # @!attribute pickup_number
   #    Pickup reference number.
   #    @return [String]
   #
   class PickupResponse < Response
-    attr_reader :pickup_number
+    attr_reader :labels, :pickup_number
   end
 end
