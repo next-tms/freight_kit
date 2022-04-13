1c5a32bee5ef07a298a7d17f217a8e6d14def1f7
Brody <brodyhoskins@users.noreply.github.com>
2022-04-13 13:11:15 -0700

    Add `DocumentResponse`

--- lib/interstellar/models.rb
+++ lib/interstellar/models.rb
@@ -6,6 +6,7 @@ require 'interstellar/models/rate'
 require 'interstellar/models/shipment'
 
 require 'interstellar/models/date_time'
+require 'interstellar/models/document_response'
 require 'interstellar/models/response'
 require 'interstellar/models/pickup_response'
 require 'interstellar/models/rate_response'
