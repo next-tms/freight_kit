43ef1a08a90b608aa8f35d9ef21c71241d0a21d9
Brody <brodyhoskins@users.noreply.github.com>
2022-12-09 13:25:52 -0800

    Add MTVL (#122)

---
+++ lib/interstellar-next/carriers/mtvl.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module Interstellar
+  class MTVL < TheGreatInformationFactory
+    class << self
+      def maximum_height
+        Measured::Length.new(105, :inches)
+      end
+
+      def maximum_weight
+        Measured::Weight.new(10_000, :pounds)
+      end
+
+      def minimum_length_for_overlength_fees
+        Measured::Length.new(8, :feet)
+      end
+
+      def pod_implemented?
+        false
+      end
+
+      def scanned_bol_implemented?
+        false
+      end
+    end
+
+    REACTIVE_FREIGHT_CARRIER = true
+
+    cattr_reader :name, :scac
+    @@name = 'GLS Freight'
+    @@scac = 'MTVL'
+
+    def build_soap_header
+      soap_header = super
+      soap_header[:password] = soap_header[:password]&.downcase
+
+      soap_header
+    end
+  end
+end
