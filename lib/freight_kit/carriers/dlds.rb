afda231e1aca1622201383d362bce40bbab8be9e
Brody Hoskins <brodyhoskins@users.noreply.github.com>
2025-03-06 18:47:51 -0800

    Add `DLDS` (#260)

--- lib/freight_kit/carriers/dlds.rb
+++ lib/freight_kit/carriers/dlds.rb
@@ -33,22 +33,6 @@ module FreightKit
       [].tap do |builder|
         builder << 'SS'
         builder << 'HAZ' if shipment.packages.any?(&:hazmat?)
-
-        if shipment.accessorials.present?
-          builder << 'RESDEL' if shipment.accessorials.include?(:residential_delivery)
-          builder << 'RESPIC' if shipment.accessorials.include?(:residential_pickup)
-        end
-
-        # @todo Update after determining why this doesn't generate resulting `chrg`
-
-        # longest_dimension = shipment.packages.map { |package| [package.length(:in), package.width(:in)].max }.max.ceil
-
-        # case longest_dimension
-        # when (96..143) then builder << 'OVER8'
-        # when (144..191) then builder << 'OVER12'
-        # when (192..239) then builder << 'OVER16'
-        # when (240..323) then builder << 'OVER20'
-        # end
       end
     end
 
